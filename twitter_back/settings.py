8c237eb36d85993397cffdc0931e593bdb14d770
MikaelReis <mikaelreis08@gmail.com>
2025-05-12 14:58:08 -0300

    preparando ambiente para pythonanywhere

--- twitter_back/settings.py
+++ twitter_back/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3i96pmw183(#@fes!fwnf)ex(&3mub-6ij*1v4&!*lwr%3mljv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'MikaelReis.pythonanywhere.com']
 
